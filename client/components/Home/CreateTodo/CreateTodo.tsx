5a29b3a4c5239098f0a26651d8e8ca609d19b4cf
Vladimir Timotijevic <timotijevicvlada@gmail.com>
2023-03-22 13:05:25 +0100

    Errors handling

--- client/components/Home/CreateTodo/CreateTodo.tsx
+++ client/components/Home/CreateTodo/CreateTodo.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState, ChangeEvent } from 'react';
 import css from "./CreateTodo.module.scss";
 import axios from 'axios';
 import { Routes } from '@/api/routes';
+import { useSnackbar } from 'notistack';
 
 //react query
 import { useMutation, useQueryClient } from 'react-query';
@@ -12,8 +13,10 @@ import { CreateInputsProps, CreateTodoProps } from '@/types/todos';
 const CreateTodo: FC<CreateTodoProps> = ({ setCreateInputs }) => {
 
     const queryClient = useQueryClient();
+    const { enqueueSnackbar } = useSnackbar();
 
     const [inputs, setInputs] = useState<CreateInputsProps>({ title: "", description: "" })
+    const [error, setError] = useState({ title: false, description: false })
 
     const { mutate: createTodo } = useMutation({
         mutationFn: (data: CreateInputsProps) => axios.post(Routes.createTodo, data),
@@ -23,6 +26,10 @@ const CreateTodo: FC<CreateTodoProps> = ({ setCreateInputs }) => {
             );
             setCreateInputs(false);
             setInputs({ title: "", description: "" });
+            enqueueSnackbar("Todo successfully created", {
+                variant: "success",
+                autoHideDuration: 3000
+            });
         },
         onError: (data) => {
             console.error(data)
@@ -30,12 +37,26 @@ const CreateTodo: FC<CreateTodoProps> = ({ setCreateInputs }) => {
     })
 
     const handleSubmit = () => {
-        if (!inputs.title || !inputs.description) return;
+        if (!inputs.title.trim()) setError(prev => ({ ...prev, title: true }));
+        if (!inputs.description.trim()) setError(prev => ({ ...prev, description: true }));
+        if (!inputs.title.trim() || !inputs.description.trim()) return;
         createTodo(inputs);
     }
 
     const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         setInputs(prev => ({ ...prev, [e.target.name]: e.target.value }));
+        if (e.target.name === "title" && e.target.value.trim()) {
+            setError(prev => ({ ...prev, title: false }))
+        }
+        if (e.target.name === "description" && e.target.value.trim()) {
+            setError(prev => ({ ...prev, description: false }))
+        }
+    }
+
+    const handleCancel = () => {
+        setCreateInputs(false);
+        setInputs({ title: "", description: "" });
+        setError({ title: false, description: false });
     }
 
     return (
@@ -46,15 +67,17 @@ const CreateTodo: FC<CreateTodoProps> = ({ setCreateInputs }) => {
                 value={inputs.title}
                 placeholder='Write your title'
                 onChange={handleChange}
+                className={error.title ? css.error : ""}
             />
             <textarea
                 name="description"
                 placeholder='Write description'
                 value={inputs.description}
                 onChange={handleChange}
+                className={error.description ? css.error : ""}
             />
             <div className={css.createButtonWrapper}>
-                <button className={css.cancel} onClick={() => setCreateInputs(false)}>
+                <button className={css.cancel} onClick={handleCancel}>
                     Cancel
                 </button>
                 <button className={css.create} onClick={handleSubmit}>
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { SnackbarProvider } from "notistack";
 
 import Layout from '@/components/Layout/Layout'
 
@@ -9,9 +10,11 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <SnackbarProvider preventDuplicate>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SnackbarProvider>
     </QueryClientProvider>
   )
 }
