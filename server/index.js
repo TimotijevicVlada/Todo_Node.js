11195ef33bde2b31b3dabcb727ded5eebbb09d51
Vladimir Timotijevic <timotijevicvlada@gmail.com>
2023-03-26 15:39:19 +0200

    Adding multer to the project

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const todosRoute = require("./routes/todos");
+const multer = require("multer");
 const cors = require('cors');
 
 dotenv.config();
 app.use(express.json());
 app.use(cors());   //I use this because of the CORS policy to enable http request work
 
+//mongoose
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -16,6 +18,21 @@ mongoose.connect(process.env.MONGO_URL, {
     .then(console.log("Connected to MongoDB"))
     .catch((err) => console.log(err))
 
+//Uploading image
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images")
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name)
+    }
+})
+const upload = multer({ storage: storage });
+app.post("/server/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
+
+//Routes to use
 app.use("/server", todosRoute)
 
 app.listen(process.env.PORT || 5000, () => {
